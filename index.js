d3a894463362ba4da3393affabb3e6a3c749ad4d
Fulcrum <joelchicano70@gmail.com>
2026-04-13 17:19:30 +0200

    Puerto redefinido para Render

--- index.js
+++ index.js
@@ -43,4 +43,5 @@ app.post('/compile', async (req, res) => {
   }
 })
 
-app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'))
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`))
